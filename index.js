159eede21f5cbf48a5ae813b344e09b4005f9249
Akshay Chhetri <chhetri.akshay@outlook.com>
2025-04-17 18:15:07 +0530

    Update index.js with CORS fix

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const bookList = require("./models/books.models")
 const express = require("express")
 const app = express()
 app.use(express.json())
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 
 initializeDatabase()
